78a88b7b306b29c20c0b3f00eea2540a09104f20
AxelAdrian1403 <flores.axeladrian@gmail.com>
2023-02-13 19:37:30 -0600

    Practica-Modulo-5
    - Probando las funciones del GameLoop y los diferentes tipos de Logs existentes

---
+++ ProyectoInicialEBAC/Assets/Scripts/PrimerEjemplo.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerEjemplo : MonoBehaviour
+{
+    //int x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //x = 0;
+        Debug.LogError("Algo salió mal"); //Log a nivel error
+        Debug.LogWarning("Algo salió medianamente mal"); //Log a nivel warning
+        print("Algo pasó"); //Igual a la función log, siempre y cuando tenga la herencia de MonoBehaviour
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+            x += 1;
+            Debug.Log("Información"); //Log a nivel información
+            Debug.Log("x -> " + x);
+        */
+        Debug.Log("Hola desde Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde FixedUpdate cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde LateUpdate");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("El objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("El objeto ha sido inhabilitado");
+    }
+}
